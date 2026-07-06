6fd7120cdc039b8e8d1561b2752cce6abbfa5b9d
khowellmobile <khowellmobile@gmail.com>
2026-07-06 15:23:54 -0400

    Changed Header and ProfileDropdown for responsiveness

--- frontend/src/layouts/layout-sections/Header.jsx
+++ frontend/src/layouts/layout-sections/Header.jsx
@@ -1,19 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import classes from "./Header.module.css";
 
+import searchIcon from "../../assets/light-notification-icon.svg";
 import DashIcon from "../../assets/dash-icon.svg";
 import SearchBox from "../../components/utilities/SearchBox";
 import Dropdown from "../../components/utilities/Dropdown";
-import ProfileDropdown from "../../components/misc/ProfileDropdown";
+import HeaderDropdown from "../../components/misc/HeaderDropdown";
 import Menu from "../../components/misc/Menu";
 
 const Header = () => {
+    const navigate = useNavigate();
+
     return (
         <div className={classes.mainContainer}>
-            <div className={classes.left}>
-                <div className={classes.icon}>
+            <div className={classes.left} >
+                <div className={classes.icon} onClick={() => navigate("/app/home")}>
                     <img src={DashIcon} alt="Icon" />
                 </div>
-                <p>
+                <p onClick={() => navigate("/app/home")}>
                     <strong>React</strong>App
                 </p>
             </div>
@@ -21,7 +25,8 @@ const Header = () => {
                 <Menu />
             </div>
             <div className={classes.right}>
-                <ProfileDropdown />
+                <img src={searchIcon} className={classes.searchIcon} onClick={() => navigate("/app/search")}/>
+                <HeaderDropdown />
             </div>
         </div>
     );
